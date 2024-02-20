d94b720e974b358b1d60e9364ca98739a59be336
Dhruv <ddrathod510@gmail.com>
2024-02-20 12:16:48 -0500

    break and continue

--- demo.py
+++ demo.py
@@ -1,54 +1,8 @@
-# String methods
-# 1. **capitalize()**: Returns a copy of the string with the first character capitalized and the rest lowercase.
-#
-# 2. **casefold()**: Returns a lowercase version of the string. It is similar to lower(), but more aggressive in converting characters to lowercase.
-#
-# 3. **center(width[, fillchar])**: Returns a centered string of length width. If fillchar is provided, it fills the remaining space with it.
-#
-# 4. **count(sub[, start[, end]])**: Returns the number of occurrences of substring sub in the string. Start and end parameters can be used to specify the search range.
-#
-# 5. **encode(encoding="utf-8", errors="strict")**: Returns an encoded version of the string using the specified encoding.
-#
-# 6. **endswith(suffix[, start[, end]])**: Returns True if the string ends with the specified suffix, otherwise False.
-#
-# 7. **find(sub[, start[, end]])**: Returns the lowest index of the substring sub in the string. Returns -1 if sub is not found.
-#
-# 8. **format(*args, **kwargs)**: Formats the string using the placeholders and values provided as arguments.
-#
-# 9. **index(sub[, start[, end]])**: Like find(), but raises ValueError if the substring is not found.
-#
-# 10. **isalnum()**: Returns True if all characters in the string are alphanumeric (letters or numbers), otherwise False.
-#
-# 11. **isalpha()**: Returns True if all characters in the string are alphabetic (letters), otherwise False.
-#
-# 12. **isdigit()**: Returns True if all characters in the string are digits, otherwise False.
-#
-# 13. **islower()**: Returns True if all characters in the string are lowercase, otherwise False.
-#
-# 14. **isspace()**: Returns True if all characters in the string are whitespace, otherwise False.
-#
-# 15. **istitle()**: Returns True if the string is titlecased (all words start with uppercase and the rest are lowercase), otherwise False.
-#
-# 16. **isupper()**: Returns True if all characters in the string are uppercase, otherwise False.
-#
-# 17. **join(iterable)**: Joins the elements of the iterable (such as a list) with the string as a separator.
-#
-# 18. **lower()**: Returns a copy of the string with all characters converted to lowercase.
-#
-# 19. **upper()**: Returns a copy of the string with all characters converted to uppercase.
-#
-# 20. **strip([chars])**: Returns a copy of the string with leading and trailing characters removed. If chars is provided, it specifies the characters to be removed.
-#
-# 21. **replace(old, new[, count])**: Returns a copy of the string with occurrences of substring old replaced by new. If count is provided, only the first count occurrences are replaced.
-#
-# 22. **split(sep=None, maxsplit=-1)**: Splits the string into a list of substrings using the specified separator sep. maxsplit specifies the maximum number of splits.
-#
-# 23. **startswith(prefix[, start[, end]])**: Returns True if the string starts with the specified prefix, otherwise False.
-#
-# 24. **title()**: Returns a titlecased version of the string (words start with uppercase characters, all remaining cased characters are lowercase).
-#
-# 25. **translate(table)**: Returns a copy of the string where each character has been mapped through the given translation table.
-#
-# 26. **zfill(width)**: Returns a copy of the string padded with zeros on the left to make a string of length width.
-#
-# These are just some of the methods available for manipulating strings in Python. Each method serves a specific purpose and can be used according to the requirements of your program.
+students = ["Dhruvraj", "Tathya", "Alex", "Joe", "Beno", "Conci", "Kalpesh"]
+
+for s in students:
+    if s == "Conci":
+        break
+    if s == "Alex":
+        continue
+    print(s)
