4ec772b6381d9cc276c3c87cef4f0ceda384f5b1
Dhruv <ddrathod510@gmail.com>
2024-02-19 20:57:02 -0500

    Type casting methods

--- demo.py
+++ demo.py
@@ -1,2 +1,42 @@
-name = input("Enter your name : ")
-print("Your name is : " + name)
+# Converting from string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int)  # Output: 10
+
+# Converting from integer to string
+num_int = 10
+num_str = str(num_int)
+print(num_str)  # Output: "10"
+
+# Converting from string to float
+float_str = "3.14"
+num_float = float(float_str)
+print(num_float)  # Output: 3.14
+
+# Converting from float to integer
+num_float = 3.14
+num_int = int(num_float)
+print(num_int)  # Output: 3
+
+# Converting from integer to boolean
+num_int = 0
+bool_val = bool(num_int)
+print(bool_val)  # Output: False
+
+# Converting from boolean to integer
+bool_val = True
+num_int = int(bool_val)
+print(num_int)  # Output: 1
+
+
+# Type-Casting List
+#
+# int()
+# float()
+# str()
+# bool()
+# list()
+# tuple()
+# set()
+# dict()
+
