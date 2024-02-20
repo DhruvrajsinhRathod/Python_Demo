1bf626a00f1d8f0c66d14ffc7160c6e3e0bb2d98
Dhruv <ddrathod510@gmail.com>
2024-02-20 10:43:14 -0500

    if else

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
-a = 5
-b = 10
+name = input("What is your name : ")
+age = input(name + ", What is your age : ")
+age = int(age)
 
-print(a>b or a<b)
-print(a>b and a<b)
-print(a>b)
-print(not a>b)
+if age>=18:
+    print(name + ", You are adult!")
+
+else:
+    print(name + ", Wait for few more years!")
