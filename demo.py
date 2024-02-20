11c364c07c4cb72e3f61bfe97d701abfdebf7347
Dhruv <ddrathod510@gmail.com>
2024-02-20 10:45:19 -0500

    added elif

--- demo.py
+++ demo.py
@@ -5,5 +5,8 @@ age = int(age)
 if age>=18:
     print(name + ", You are adult!")
 
+elif age<18 and age>3:
+    print(name + ", Focus on studies!")
+
 else:
     print(name + ", Wait for few more years!")
