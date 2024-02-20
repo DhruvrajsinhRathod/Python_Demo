d4fca7628aa4f6b32a3f2a21e2496f19a51640a4
Dhruv <ddrathod510@gmail.com>
2024-02-20 12:09:55 -0500

    list

--- demo.py
+++ demo.py
@@ -1,2 +1,23 @@
-for i in range(5):
-    print(i+1)
+marks = [1,2,3,4,5,90809,32487]
+
+print(marks)
+print(marks[0])
+print(marks[-1])
+print(marks[1:4])
+
+for score in marks:
+    print(score)
+
+marks.append(478)
+marks.insert(1,213)
+print(marks)
+print(len(marks))
+
+i = 0
+while i < len(marks):
+    print(marks[i])
+    i = i+1
+
+print(marks)
+marks.clear()
+print(marks)
