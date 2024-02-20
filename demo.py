f6bdd31f18c708a403117a7f1e861706efcf3653
Dhruv <ddrathod510@gmail.com>
2024-02-19 21:18:16 -0500

    Type Casting

--- demo.py
+++ demo.py
@@ -1,42 +1,5 @@
-# Converting from string to integer
-num_str = "10"
-num_int = int(num_str)
-print(num_int)  # Output: 10
+first = input("Enter your first number : ")
+second = input("Enter your second number : ")
 
-# Converting from integer to string
-num_int = 10
-num_str = str(num_int)
-print(num_str)  # Output: "10"
-
-# Converting from string to float
-float_str = "3.14"
-num_float = float(float_str)
-print(num_float)  # Output: 3.14
-
-# Converting from float to integer
-num_float = 3.14
-num_int = int(num_float)
-print(num_int)  # Output: 3
-
-# Converting from integer to boolean
-num_int = 0
-bool_val = bool(num_int)
-print(bool_val)  # Output: False
-
-# Converting from boolean to integer
-bool_val = True
-num_int = int(bool_val)
-print(num_int)  # Output: 1
-
-
-# Type-Casting List
-#
-# int()
-# float()
-# str()
-# bool()
-# list()
-# tuple()
-# set()
-# dict()
-
+sum = int(first) + int(second)
+print(sum)
