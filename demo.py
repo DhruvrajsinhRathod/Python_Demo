63d7592d2bfec03603a9666020e85054e468d8a9
Dhruv <ddrathod510@gmail.com>
2024-02-20 12:34:33 -0500

    notes

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
-# Tuples are immutable and lists are mutable
-# list []
-# tuple ()
+names = "Dhruvraj", "Tathya", "Joe", "Alex"
+print(type(names))
+# If there is no ({[ then it automatically becomes tuple in python.
+# Tuple starts from ()
 
-marks = (1, 2, 123, 12, 3, 21, 12, 3124523, 46867, 645352, 2)
+something = {1, 2, 3, 13, 1, 132, "D", "D"}
+print(something)
+print(type(something))
 
-# count(value): Returns the number of occurrences of a specified value in the tuple.
-# index(value): Returns the index of the first occurrence of a specified value in the tuple.
+# repetition gets deleted in set
+# no concept of index in set
