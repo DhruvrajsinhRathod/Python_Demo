ff92130ebb6788a0acc28e8238227a71ad3b8939
Dhruv <ddrathod510@gmail.com>
2024-02-20 12:22:38 -0500

    tuple

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
-students = ["Dhruvraj", "Tathya", "Alex", "Joe", "Beno", "Conci", "Kalpesh"]
+# Tuples are immutable and lists are mutable
+# list []
+# tuple ()
 
-for s in students:
-    if s == "Conci":
-        break
-    if s == "Alex":
-        continue
-    print(s)
+marks = (1, 2, 123, 12, 3, 21, 12, 3124523, 46867, 645352, 2)
+
+# count(value): Returns the number of occurrences of a specified value in the tuple.
+# index(value): Returns the index of the first occurrence of a specified value in the tuple.
