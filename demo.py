3e0221ff18fa2dc2df8acbc7388868fc84af9cb1
Dhruv <ddrathod510@gmail.com>
2024-02-20 12:11:38 -0500

    list methods

--- demo.py
+++ demo.py
@@ -1,23 +1,21 @@
-marks = [1,2,3,4,5,90809,32487]
-
-print(marks)
-print(marks[0])
-print(marks[-1])
-print(marks[1:4])
-
-for score in marks:
-    print(score)
-
-marks.append(478)
-marks.insert(1,213)
-print(marks)
-print(len(marks))
-
-i = 0
-while i < len(marks):
-    print(marks[i])
-    i = i+1
-
-print(marks)
-marks.clear()
-print(marks)
+# 1. **append(x)**: Adds an element x to the end of the list.
+# list methods
+# 2. **extend(iterable)**: Extends the list by appending elements from the iterable.
+#
+# 3. **insert(i, x)**: Inserts an element x at the specified index i.
+#
+# 4. **remove(x)**: Removes the first occurrence of element x from the list.
+#
+# 5. **pop([i])**: Removes and returns the element at index i. If i is not provided, removes and returns the last element.
+#
+# 6. **clear()**: Removes all elements from the list.
+#
+# 7. **index(x[, start[, end]])**: Returns the index of the first occurrence of element x in the list. Optionally, start and end parameters can be used to specify the search range.
+#
+# 8. **count(x)**: Returns the number of occurrences of element x in the list.
+#
+# 9. **sort(key=None, reverse=False)**: Sorts the elements of the list in place. The key parameter specifies a function to be called on each element for sorting, and reverse specifies whether to sort in descending order.
+#
+# 10. **reverse()**: Reverses the elements of the list in place.
+#
+# 11. **copy()**: Returns a shallow copy of the list.
